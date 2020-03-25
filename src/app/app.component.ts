0bef3fd53f7851cec91478e244341b16583ed1b2
Morgan <ketzatl@hotmail.fr>
2020-03-25 11:51:52 +0100

    - Integration de Firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'immoAngular';
+  title = 'immo-Angular8';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDYdY2qYONGWpHbtfd_6_jCFJs-ztmBAn8',
+      authDomain: 'immo-angular8.firebaseapp.com',
+      databaseURL: 'https://immo-angular8.firebaseio.com',
+      projectId: 'immo-angular8',
+      storageBucket: 'immo-angular8.appspot.com',
+      messagingSenderId: '511852127769',
+      appId: '1:511852127769:web:ef4007d3d505fa631c2c88'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
+
