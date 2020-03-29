306101108cc7b8a3601c5d75395890ecc71f4c64
Morgan <ketzatl@hotmail.fr>
2020-03-29 18:22:12 +0200

    - Gestion des liaisons routes/button. --> Modifs. app-routing.module.ts & header.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
-import {SigninComponent} from "./authentication/signin/signin.component";
+import {SigninComponent} from './authentication/signin/signin.component';
 
 
 const routes: Routes = [
